68cce7bc11e45172e039bac1158929910f334b25
lexx54 <chapelexx54@gmail.com>
2020-12-10 15:38:20 -0400

    improved the JS and css

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 document.addEventListener("click",e=>{
-  if (e.target.matches(".container" || ".turn-off")){
-    let cont=document.querySelector("aside");
-    cont.parentNode.removeChild(cont)
-    
-  }
+  let $box=document.querySelector("aside")
+  if ($box) $box.parentNode.removeChild($box)
 })

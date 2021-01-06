31fd5338955a8f10f04f33177535a8724116df8f
lexx54 <chapelexx54@gmail.com>
2021-01-06 19:16:30 -0400

    added a carrousel and back and forward functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+const d=document,
+  $gallery=d.querySelector(".project");
+
+  let position=0
+
+
+d.addEventListener("click",e=>{
+  
+  if (e.target.matches(".previous")){
+    if (position<0){
+      position+=220;
+      $gallery.style.transform=`translateX(${position}px)`;
+    }
+  }
+  if (e.target.matches(".next")){
+    if (position>-880){
+      position-=220;
+      $gallery.style.transform=`translateX(${position}px)`;
+    }
+  }
+})

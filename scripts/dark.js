2c9115971460f72977843b6155ef889e6195b42f
lexx54 <chapelexx54@gmail.com>
2021-01-26 22:05:33 -0400

    finished dark theme btn design and functionality

---
+++ scripts/dark.js
@@ -0,0 +1,6 @@
+const d=document;
+
+export function btnChange(){
+  const $btn=d.querySelector('.btn-circle');
+  $btn.classList.toggle("btn-move")
+}
--- scripts/theme.js
+++ scripts/theme.js
@@ -1,32 +1,50 @@
 const d=document,
 ls=localStorage;
 
-export function theme(dataType){
+export function changeMode(dataType){
   const $text=d.querySelectorAll(`[${dataType}='letter']`),
   $background=d.querySelectorAll(`[${dataType}="background"]`);
   
-  if(ls.getItem("theme")===null) ls.setItem("theme","light");
-  if(ls.getItem("theme")==="light") lightMode($text,$background);
-  if(ls.getItem("theme")==="dark") darkMode($text,$background);
- 
-  console.log($background)
+  if(ls.getItem("theme")==="light") {
+    localStorage.setItem("theme",'dark')
+    darkMode($text,$background);
+    btnChange()
+  }else if(ls.getItem("theme")==="dark") {
+    localStorage.setItem("theme",'light')
+    lightMode($text,$background);
+    btnChange()
+  };
+}
+
+export function setMode(dataType){
+  let mode=localStorage.getItem("theme")?localStorage.getItem("theme"):(localStorage.setItem("theme","light"),"light");
+  const $text=d.querySelectorAll(`[${dataType}='letter']`),
+  $background=d.querySelectorAll(`[${dataType}="background"]`);
+
+  if(mode==="light"){
+    lightMode($text,$background);
+    btnChange()
+  } else if (mode==="dark"){
+    darkMode($text,$background)
+    btnChange();
+  };
 }
 
 function lightMode(t,b){
-  ls.setItem("theme","light");
-  t.forEach(ele=>{
-    ele.style.color="white";
-  })
-  b.forEach(ele=>{
-    ele.style.backgroundColor='white';
-  })
+  t.forEach(ele=> ele.style.color="white");
+  b.forEach(ele=> ele.style.backgroundColor='white');
 }
 function darkMode(t,b){
-  ls.setItem("theme","dark");
-  t.forEach(ele=>{
-    ele.style.color="black";
-  })
-  b.forEach(ele=>{
-    ele.style.backgroundColor='black';
-  })
+  t.forEach(ele=> ele.style.color="black");
+  b.forEach(ele=> ele.style.backgroundColor='black');
+}
+function btnChange(){
+  const $btn=d.querySelector('.btn-circle');
+  if(ls.getItem("theme")==="dark") {
+    $btn.classList.add("btn-move")
+    $btn.innerHTML="☀️";
+  } else {
+    $btn.classList.remove("btn-move")
+    $btn.textContent="🌙";
+  };
 }

14cb3c74f5e13e9758707f95db0b541b7c5096c4
lexx54 <chapelexx54@gmail.com>
2021-08-15 16:57:39 -0400

    fixed projects card slide when scrolling down

--- scripts/observer.js
+++ scripts/observer.js
@@ -1,24 +1,42 @@
 const d=document;
 
-export function observer(){
-  const $section=d.querySelectorAll(".skills-container");
+// export function observer(){
+//   const $section = d.querySelectorAll(".project__container");
+//   const callback = entries => {
+//     entries.forEach( entry => {
+//       if (entry.isIntersecting){
+//         console.log(entry);
+//         console.log(entry.target);
+//         entry.target.onscroll = (ev) => {
+//           console.log(ev)
+//         }
+//       }
+//     })
+//   };
+//   const observer = new IntersectionObserver(callback,{threshold:0.9});
+//   $section.forEach( section => observer.observe(section)); 
+// };
 
-  const cb=(entries)=>{
-    entries.forEach(entry=>{
-      if(entry.isIntersecting){
-        for (const ele of entry.target.children){
-          ele.classList.add("skills-box-move");
-        }
-      } else{
-        for (const ele of entry.target.children){
-          ele.classList.remove("skills-box-move");
-        }
-      }
-    })
-  }
-    const observer=new IntersectionObserver(cb,{threshold:0.3});
-    $section.forEach(box=>observer.observe(box))
-}
+
+// export function observer(){
+//   const $section=d.querySelectorAll(".skills-container");
+
+//   const cb=(entries)=>{
+//     entries.forEach(entry=>{
+//       if(entry.isIntersecting){
+//         for (const ele of entry.target.children){
+//           ele.classList.add("skills-box-move");
+//         }
+//       } else{
+//         for (const ele of entry.target.children){
+//           ele.classList.remove("skills-box-move");
+//         }
+//       }
+//     })
+//   }
+//     const observer=new IntersectionObserver(cb,{threshold:0.3});
+//     $section.forEach(box=>observer.observe(box))
+// }
   
 
 
---
+++ scripts/scrolling.js
@@ -0,0 +1,55 @@
+const scrollDown = (space,container,list,title,current) => {
+    title.style.transform = `translateY(${current-300}px)`;
+    container[0].style.transform = `translateY(${current-300}px)`;
+    for (let i = 1; i < container.length; i++){
+      container[i].style.transform = `translateY(${current-300 - space}px)`;
+    } 
+    if(space >=500 ) container[1].style.transform = `translateY(${current-750}px)`
+    if(space >=900 ) container[2].style.transform = `translateY(${current-1200}px)`
+    if(space >=1400 ) container[3].style.transform = `translateY(${current-1650}px)`
+    if(space >=1900 ) container[4].style.transform = `translateY(${current-2100}px)`
+    if(space >=2300 ) container[5].style.transform = `translateY(${current-2550}px)`
+  }
+
+const scrollUp = (space,container,list,title,current) => {
+  title.style.transform = `translateY(${current-300}px)`;
+  container[0].style.transform = `translateY(${current-300}px)`;
+
+  for (let i = 1; i < container.length; i++){
+    container[i].style.transform = `translateY(${current-300 - space}px)`;
+  } 
+  if(space >=500 ) container[1].style.transform = `translateY(${current-750}px)`
+  if(space >=900 ) container[2].style.transform = `translateY(${current-1200}px)`
+  if(space >=1400 ) container[3].style.transform = `translateY(${current-1650}px)`
+  if(space >=1900 ) container[4].style.transform = `translateY(${current-2100}px)`
+  if(space >=2300 ) container[5].style.transform = `translateY(${current-2550}px)`
+  if(space>=2400){
+    // list.style.height = "110vh";
+    list.style.marginBottom = "18rem";
+    container[0].style.transform = `translateY(250px)`
+    container[1].style.transform = `translateY(-200px)`
+    container[2].style.transform = `translateY(-650px)`
+    container[3].style.transform = `translateY(-1100px)`
+    container[4].style.transform = `translateY(-1550px)`
+    container[5].style.transform = `translateY(-2000px)`
+  }
+
+
+  // for (let i = 1; i < container.length; i++){
+  //   container[i].style.transform = `translateY(-${space}px)` 
+  // }
+  // if(space < 450){
+  //   list.style.height = "auto";
+  // } 
+  // if (space >= 450)container[1].style.transform = `translateY(-450px)`
+  // if (space >= 900)container[2].style.transform = `translateY(-900px)`
+  // if (space >= 1350)container[3].style.transform = `translateY(-1350px)`
+  // if (space >= 1800)container[4].style.transform = `translateY(-1800px)`
+  // if (space >= 2250){
+  //   container[5].style.transform = `translateY(-2250px)`
+  //   list.style.height = "110vh";
+  //   return true
+  // }
+}
+
+export {scrollDown, scrollUp};
